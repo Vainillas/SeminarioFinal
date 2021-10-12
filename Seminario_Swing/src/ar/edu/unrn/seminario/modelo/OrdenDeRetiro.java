ebabaf41a6da39b304bd5a52300df198eee04978
mateo <mateo@DESKTOP-M9PSJAH>
2021-10-11 23:23:23 -0300

    Implementacion de orden, pedido y visita 0.5

--- Seminario_Swing/src/ar/edu/unrn/seminario/modelo/OrdenDeRetiro.java
+++ Seminario_Swing/src/ar/edu/unrn/seminario/modelo/OrdenDeRetiro.java
@@ -1,5 +1,50 @@
 package ar.edu.unrn.seminario.modelo;
 
-public class OrdenDeRetiro {
+import java.util.ArrayList;
 
+public class OrdenDeRetiro {
+	private PedidoDeRetiro pedidoAsociado;
+	private Recolector recolector;
+	private Vivienda vivienda;
+	private Estado estado;
+	private ArrayList<Visita>visitas;
+	private int codigo;
+	
+	public OrdenDeRetiro(PedidoDeRetiro p) {
+		pedidoAsociado=p;
+		recolector=null;
+		vivienda=p.getVivienda();
+		estado=new Estado();
+		visitas=new ArrayList<>();
+	}
+	public OrdenDeRetiro(PedidoDeRetiro p, Recolector r) {
+		this(p);
+		recolector=r;
+	}
+	public Recolector getRecolector() {
+		return recolector;
+	}
+	public void setRecolector(Recolector recolector) {
+		this.recolector = recolector;
+	}
+	public ArrayList<Visita> getVisitas() {
+		return visitas;
+	}
+	public void setVisitas(ArrayList<Visita> visitas) {
+		this.visitas = visitas;
+	}
+	public PedidoDeRetiro getPedidoAsociado() {
+		return pedidoAsociado;
+	}
+	public Vivienda getVivienda() {
+		return vivienda;
+	}
+	public Estado getEstado() {
+		return estado;
+	}
+	public int getCodigo() {
+		return codigo;
+	}
+	
+	
 }
