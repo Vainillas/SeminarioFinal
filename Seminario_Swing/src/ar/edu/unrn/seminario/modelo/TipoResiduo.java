69efb21c94ff729792f8ca4fa248563a2b4d5d01
santi <santi@LAPTOP-AB7LDDD1>
2021-10-16 23:00:57 -0300

    xd

--- Seminario_Swing/src/ar/edu/unrn/seminario/modelo/TipoResiduo.java
+++ Seminario_Swing/src/ar/edu/unrn/seminario/modelo/TipoResiduo.java
@@ -1,5 +1,27 @@
 package ar.edu.unrn.seminario.modelo;
 
 public class TipoResiduo {
+	private String nombre;
+	private int PuntajeXKg;
 
+	public TipoResiduo(String nombre, int puntajeXKg) {
+		this.nombre = nombre;
+		PuntajeXKg = puntajeXKg;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public int getPuntajeXKg() {
+		return PuntajeXKg;
+	}
+
+	public void setPuntajeXKg(int puntajeXKg) {
+		PuntajeXKg = puntajeXKg;
+	}
 }
