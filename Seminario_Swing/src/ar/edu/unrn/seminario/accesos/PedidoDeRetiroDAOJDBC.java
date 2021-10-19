4a8356e102463d392f0cb2477e1d10dd30ef5bcc
usuario <usuario@192.168.1.86>
2021-10-18 22:28:38 -0300

    f

--- Seminario_Swing/src/ar/edu/unrn/seminario/accesos/PedidoDeRetiroDAOJDBC.java
+++ Seminario_Swing/src/ar/edu/unrn/seminario/accesos/PedidoDeRetiroDAOJDBC.java
@@ -61,6 +61,7 @@ public class PedidoDeRetiroDAOJDBC implements PedidoDeRetiroDao{
 	            
 	            for(int i=0; i < p.getListResiduos().size() ; i++) {
 		            statement2.setInt(1, codigoPedido);
+		            System.out.println(p.getListResiduos().get(i).getTipo().getNombre());
 		            statement2.setString(2, p.getListResiduos().get(i).getTipo().getNombre());
 		            statement2.setInt(3, p.getListResiduos().get(i).getCantidadKg());
 		            statement2.executeUpdate();
