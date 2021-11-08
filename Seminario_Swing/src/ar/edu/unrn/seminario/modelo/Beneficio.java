2b93688ebe5cafc789d4185c05c27cb412a8f1e5
santi <santi@LAPTOP-AB7LDDD1>
2021-11-08 00:06:23 -0300

    Version con obtenerBeneficios

--- Seminario_Swing/src/ar/edu/unrn/seminario/modelo/Beneficio.java
+++ Seminario_Swing/src/ar/edu/unrn/seminario/modelo/Beneficio.java
@@ -8,11 +8,20 @@ import ar.edu.unrn.seminario.exceptions.NotNumberException;
 public class Beneficio {
 	private String descripcion;
 	private String puntajeConsumible;
+	private int codigo;
+
 	public Beneficio(String descripcion, String puntajeConsumible)throws NotNullException, DataEmptyException, NotNumberException {
 		validarDatos(descripcion, puntajeConsumible);
 		this.descripcion = descripcion;
 		this.puntajeConsumible = puntajeConsumible;
 	}
+	
+	public Beneficio(String descripcion, String puntajeConsumible, int codigoBeneficio){
+		this.descripcion = descripcion;
+		this.puntajeConsumible = puntajeConsumible;
+		this.codigo = codigoBeneficio;
+	}
+	
 	public void validarDatos(String descripcion, String puntajeConsumible) throws NotNullException, DataEmptyException, NotNumberException {
 		if(ConditionHelper.stringIsEmpty(puntajeConsumible)) {throw new DataEmptyException("Puntaje vacio");}
 		if(ConditionHelper.stringIsEmpty(descripcion)) {throw new NotNullException("Descripcion vacia");}
@@ -24,17 +33,29 @@ public class Beneficio {
 		
 		
 	}
+	
 	public String getDescripcion() {
 		return this.descripcion;
 	}
+	
 	public String getPuntajeConsumible() {
 		return this.puntajeConsumible;
 	}
+	
 	public void setPuntajeConsumible(String nuevoPuntaje) {
 		this.puntajeConsumible = nuevoPuntaje;
 	}
+	
 	public void setDescripcion(String nuevaDescripcion) {
 	this.descripcion = nuevaDescripcion;
+	}
+	
+	public int getCodigo() {
+		return codigo;
+	}
+
+	public void setCodigo(int codigo) {
+		this.codigo = codigo;
 	}
 
 }
