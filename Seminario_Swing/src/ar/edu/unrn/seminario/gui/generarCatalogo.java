df0e4aa7d2245013f40d381c9c3888a3fe76a5f3
usuario <usuario@192.168.1.86>
2021-11-09 22:43:11 -0300

    helpmi

--- Seminario_Swing/src/ar/edu/unrn/seminario/gui/generarCatalogo.java
+++ Seminario_Swing/src/ar/edu/unrn/seminario/gui/generarCatalogo.java
@@ -3,34 +3,45 @@ package ar.edu.unrn.seminario.gui;
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
 import java.awt.ScrollPane;
+import java.util.ArrayList;
 import java.util.List;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableModel;
 
+import ar.edu.unrn.seminario.api.IApi;
 import ar.edu.unrn.seminario.api.PersistenceApi;
 import ar.edu.unrn.seminario.dto.BeneficioDTO;
 import ar.edu.unrn.seminario.dto.DueñoDTO;
+import ar.edu.unrn.seminario.exceptions.AppException;
+import ar.edu.unrn.seminario.exceptions.DataEmptyException;
+import ar.edu.unrn.seminario.exceptions.NotNullException;
+import ar.edu.unrn.seminario.exceptions.NotNumberException;
+
 import javax.swing.JScrollPane;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import javax.swing.JButton;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 public class generarCatalogo extends JFrame {
 
 	private JPanel contentPane;
-	private DefaultTableModel modeloBeneficiosSinSeleccionar;
-	private DefaultTableModel modeloBeneficiosSeleccionados;
-	private JTable tableBeneficiosSeleccionados;
-	private JTable tableBeneficioSinSeleccionar;
-	private ScrollPane scrollPaneBeneficiosSinSeleccionar;
-	private ScrollPane scrollPaneBeneficiosSeleccionados;
+	private JTable tableBeneficiosNoAsociados;
+	private JTable tableBeneficiosAsociados;
+	private DefaultTableModel modeloBeneficiosNoAsociados;
+	private DefaultTableModel modeloBeneficiosAsociados;
 	
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					PersistenceApi api = new PersistenceApi();
+					IApi api = new PersistenceApi();
 					generarCatalogo frame = new generarCatalogo(api);
 					frame.setVisible(true);
 				} catch (Exception e) {
@@ -41,61 +52,131 @@ public class generarCatalogo extends JFrame {
 	}
 
 
-	public generarCatalogo(PersistenceApi api) {
+	public generarCatalogo(IApi api) {
+		setTitle("Generar Campaña");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 1000, 402);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
-		JPanel panel = new JPanel();
-		panel.setBounds(10, 11, 402, 262);
-		contentPane.add(panel);
-		panel.setLayout(new BorderLayout(0, 0));
-	
-		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(572, 11, 402, 262);
-		contentPane.add(panel_1);
-		panel_1.setLayout(new BorderLayout(0, 0));
-		
-		JScrollPane scrollPaneBeneficiosSinSeleccionar = new JScrollPane();
-		panel_1.add(scrollPaneBeneficiosSinSeleccionar, BorderLayout.CENTER);
-		JScrollPane scrollPaneBeneficiosSeleccionados = new JScrollPane();
-		panel.add(scrollPaneBeneficiosSeleccionados, BorderLayout.CENTER);
-		
+		JPanel panelBeneficiosNoAsociados = new JPanel();
+		panelBeneficiosNoAsociados.setBounds(10, 50, 402, 262);
+		contentPane.add(panelBeneficiosNoAsociados);
 		String [] titulos = {
 				"Descripcion",
 				"Puntaje"
 				
 		};
+		modeloBeneficiosAsociados = new DefaultTableModel(new Object[][] {}, titulos);
+		modeloBeneficiosNoAsociados = new DefaultTableModel(new Object[][] {}, titulos);
+		try {
+			List<BeneficioDTO> beneficio = api.obtenerBeneficios();
+			for(BeneficioDTO b : beneficio) {
+				
+				modeloBeneficiosNoAsociados.addRow(new Object[] {b.getDescripcion(),b.getPuntajeConsumible()});
+			}
+		} catch (AppException | NotNullException | DataEmptyException | NotNumberException e) {
+			
+			JOptionPane.showMessageDialog(null, e.getMessage(),"error",JOptionPane.ERROR_MESSAGE);
+		}
 		
-		this.modeloBeneficiosSeleccionados = new DefaultTableModel(new Object[][] {}, titulos);
-		this.modeloBeneficiosSinSeleccionar = new DefaultTableModel(new Object[][] {}, titulos);
+		
+		
+		JPanel panelBeneficiosAsociados = new JPanel();
+		panelBeneficiosAsociados.setBounds(572, 50, 402, 262);
+		contentPane.add(panelBeneficiosAsociados);
+		
+		panelBeneficiosAsociados.setLayout(new BorderLayout(0, 0));
+		
+		JScrollPane scrollPaneBeneficiosAsociados = new JScrollPane();
+		panelBeneficiosAsociados.add(scrollPaneBeneficiosAsociados, BorderLayout.CENTER);
+		
+		tableBeneficiosAsociados = new JTable();
+		tableBeneficiosAsociados.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				String  descripcion = (String)tableBeneficiosAsociados.getValueAt(tableBeneficiosAsociados.getSelectedRow(),0 );
+				String puntaje = (String)tableBeneficiosAsociados.getValueAt(tableBeneficiosAsociados.getSelectedRow(),1 );
+				//tableBeneficiosNoAsociados.clearSelection();
+				
+				modeloBeneficiosAsociados.removeRow(tableBeneficiosAsociados.getSelectedRow());
+				
+				modeloBeneficiosNoAsociados.addRow(new Object[] {descripcion,puntaje});
+				tableBeneficiosNoAsociados.setModel(modeloBeneficiosNoAsociados);
+			}
+		});
+		scrollPaneBeneficiosAsociados.setViewportView(tableBeneficiosAsociados);
+		panelBeneficiosNoAsociados.setLayout(new BorderLayout(0, 0));
+		this.tableBeneficiosAsociados.setModel(this.modeloBeneficiosAsociados);
+		
+		JScrollPane scrollPaneBeneficioNoAsociado = new JScrollPane();
+		panelBeneficiosNoAsociados.add(scrollPaneBeneficioNoAsociado, BorderLayout.CENTER);
+		tableBeneficiosNoAsociados = new JTable();
+		tableBeneficiosNoAsociados.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				String  descripcion = (String)tableBeneficiosNoAsociados.getValueAt(tableBeneficiosNoAsociados.getSelectedRow(),0 );
+				String puntaje = (String)tableBeneficiosNoAsociados.getValueAt(tableBeneficiosNoAsociados.getSelectedRow(),1 );
+				//tableBeneficiosNoAsociados.clearSelection();
+				
+				modeloBeneficiosNoAsociados.removeRow(tableBeneficiosNoAsociados.getSelectedRow());
+				
+				modeloBeneficiosAsociados.addRow(new Object[] {descripcion,puntaje});
+				tableBeneficiosAsociados.setModel(modeloBeneficiosAsociados);
+
+				
+			}
+		});
+		
+		tableBeneficiosNoAsociados.setModel(this.modeloBeneficiosNoAsociados);
+		scrollPaneBeneficioNoAsociado.setViewportView(tableBeneficiosNoAsociados);
+		
+		JLabel lbBeneficiosSinAsociar = new JLabel("beneficios sin Asociar");
+		lbBeneficiosSinAsociar.setHorizontalAlignment(SwingConstants.CENTER);
+		lbBeneficiosSinAsociar.setBounds(141, 25, 168, 14);
+		contentPane.add(lbBeneficiosSinAsociar);
+		
+		JLabel lbBeneficiosAsociados = new JLabel("Beneficios Asociados ");
+		lbBeneficiosAsociados.setHorizontalAlignment(SwingConstants.CENTER);
+		lbBeneficiosAsociados.setBounds(678, 25, 199, 14);
+		contentPane.add(lbBeneficiosAsociados);
+		
+		JPanel panelBotones = new JPanel();
+		panelBotones.setBounds(422, 50, 140, 262);
+		contentPane.add(panelBotones);
+		
+		JButton btnAgregarBeneficio = new JButton("AgregarBeneficio");
+		btnAgregarBeneficio.addActionListener((e)->{
+			if(btnAgregarBeneficio.isEnabled()) {
+				
+			}
+			
+		});
+		
+		panelBotones.add(btnAgregarBeneficio);
+		
 		
 		
 		
 		
 		
 
+
 		
-		this.tableBeneficioSinSeleccionar.setModel(modeloBeneficiosSinSeleccionar);
-		this.tableBeneficiosSeleccionados.setModel(modeloBeneficiosSeleccionados);
-		scrollPaneBeneficiosSinSeleccionar.setViewportView(this.tableBeneficioSinSeleccionar);
-		scrollPaneBeneficiosSeleccionados.setViewportView(this.tableBeneficiosSeleccionados);
+
 	}
 	private void reloadGrid(List<BeneficioDTO> beneficio ) {
-		this.modeloBeneficiosSeleccionados.setRowCount(0);
+		/*this.modeloBeneficiosSeleccionados.setRowCount(0);
 		
 			for (BeneficioDTO b : beneficio) {
 				this.modeloBeneficiosSeleccionados.addRow(new Object[] { 
 						"d",
 						
 					
-				});
+				});*/
 				
 			
 	}
-			
 	}
-}
+
